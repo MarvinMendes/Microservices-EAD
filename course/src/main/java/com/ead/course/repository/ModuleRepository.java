e80caa138e228752470bd01fef5f2b75b62ebb74
MarvinMendes <marvin.m.lopes@gmail.com>
2025-09-03 09:50:39 -0300

    Modificações de consultas com @Fetch e @EntityGraph.

--- course/src/main/java/com/ead/course/repository/ModuleRepository.java
+++ course/src/main/java/com/ead/course/repository/ModuleRepository.java
@@ -1,9 +1,15 @@
 package com.ead.course.repository;
 
 import com.ead.course.model.ModuleModel;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface ModuleRepository extends JpaRepository<ModuleModel, UUID> {
+
+    @EntityGraph(attributePaths = {"course"})
+    ModuleModel findByTitle(String title);
+
+
 }
